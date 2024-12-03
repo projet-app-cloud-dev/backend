c98085c506441de5a30acd01fe1bc6f26c59edbf
oupson <oupson1er@gmail.com>
2024-12-03 11:39:14 +0100

    Fix test for auth.

--- cards/build.gradle.kts
+++ cards/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("com.h2database:h2:2.3.232")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
