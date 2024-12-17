87e13a0fe5b4a3837618e87713762561521d1dcd
oupson <oupson1er@gmail.com>
2024-12-17 20:12:40 +0100

    Fix jwt configuration in collection.

--- collection/src/main/kotlin/fr/pokecloud/collection/config/SecurityConfig.kt
+++ collection/src/main/kotlin/fr/pokecloud/collection/config/SecurityConfig.kt
@@ -1,30 +1,67 @@
 package fr.pokecloud.collection.config
 
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.jwk.JWK
+import com.nimbusds.jose.jwk.OctetSequenceKey
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint
+import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler
 import org.springframework.security.web.SecurityFilterChain
+import java.security.Key
+import javax.crypto.spec.SecretKeySpec
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http
-            .csrf { it.disable() }
-            .authorizeHttpRequests { requests ->
-                requests
-                    .requestMatchers(HttpMethod.GET, "/**").permitAll()
-                    .requestMatchers(HttpMethod.POST, "/**").authenticated()
-                    .requestMatchers(HttpMethod.DELETE, "/**").authenticated()
-                    .requestMatchers("/swagger-ui/**")
-                    .permitAll().requestMatchers("/swagger-ui.html").permitAll().requestMatchers("/v3/api-docs*/**")
-                    .permitAll()
-                    .anyRequest().denyAll()
-            }
+        http.csrf { it.disable() }.authorizeHttpRequests { requests ->
+            requests.requestMatchers(HttpMethod.GET, "/**").permitAll().requestMatchers(HttpMethod.POST, "/**")
+                .authenticated().requestMatchers(HttpMethod.DELETE, "/**").authenticated()
+                .requestMatchers("/swagger-ui/**").permitAll().requestMatchers("/swagger-ui.html").permitAll()
+                .requestMatchers("/v3/api-docs*/**").permitAll().anyRequest().denyAll()
+        }.sessionManagement { session ->
+            session.sessionCreationPolicy(
+                SessionCreationPolicy.STATELESS
+            )
+        }.oauth2ResourceServer { oauth2 ->
+            oauth2.jwt(
+                Customizer.withDefaults()
+            )
+        }.exceptionHandling { exceptions: ExceptionHandlingConfigurer<HttpSecurity?> ->
+            exceptions.authenticationEntryPoint(BearerTokenAuthenticationEntryPoint())
+                .accessDeniedHandler(BearerTokenAccessDeniedHandler())
+        }
         return http.build()
     }
 
+    @Bean
+    fun jwtDecoder(jwk: JWK): JwtDecoder {
+        return NimbusJwtDecoder.withSecretKey(jwk.toOctetSequenceKey().toSecretKey()).build()
+    }
+
+    @OptIn(ExperimentalEncodingApi::class)
+    @Bean
+    fun getJwk(environment: Environment): JWK {
+        val key: Key = SecretKeySpec(
+            Base64.decode(
+                requireNotNull(environment.getProperty("jwt.key")) {
+                    "Missing JWT_KEY environment variable"
+                }), JWSAlgorithm.HS256.name
+        )
+        val jwk: JWK = OctetSequenceKey.Builder(key.encoded).algorithm(JWSAlgorithm.HS256).build()
+        return jwk
+    }
 }
