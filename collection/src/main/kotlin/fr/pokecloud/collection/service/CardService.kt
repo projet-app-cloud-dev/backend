8c118ce1c352981bbfcbb95a7f2d71a8818522ba
oupson <oupson1er@gmail.com>
2024-12-17 13:25:19 +0100

    Implement add and remove cards (untested).

--- collection/src/main/kotlin/fr/pokecloud/collection/service/CardService.kt
+++ collection/src/main/kotlin/fr/pokecloud/collection/service/CardService.kt
@@ -1,9 +1,38 @@
 package fr.pokecloud.collection.service
 
+import fr.pokecloud.collection.database.repository.CardRepository
 import fr.pokecloud.collection.model.Card
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClient
 
-class CardService {
-    fun getCard(cardId : Long) : Card {
-        TODO()
+@Service
+class CardService(
+    private val cardRepository: CardRepository
+) {
+    data class CardName(
+        val id: Int, val name: String
+    )
+
+    private val restClient = RestClient.builder().baseUrl("http://cards").build()
+
+    fun getOrInsertCard(cardId: Long): Card {
+        val card = cardRepository.findByIdOrNull(cardId)?.let {
+            Card(it.id, it.name)
+        }
+        if (card != null) {
+            return card
+        }
+
+        val cardName = restClient.get().uri("/${cardId}").accept(MediaType.APPLICATION_JSON).retrieve()
+            .body(CardName::class.java)!!.name
+        return cardRepository.save(
+            fr.pokecloud.collection.database.entities.Card(
+                cardId, cardName, listOf()
+            )
+        ).let {
+            Card(it.id, it.name)
+        }
     }
 }
