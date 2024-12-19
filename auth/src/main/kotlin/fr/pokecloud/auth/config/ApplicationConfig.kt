abfa517d9d47f1517614a52a047ae4574bfe12ef
oupson <oupson1er@gmail.com>
2024-12-19 16:53:08 +0100

    Expose api docs when behind docker.

--- auth/src/main/kotlin/fr/pokecloud/auth/config/ApplicationConfig.kt
+++ auth/src/main/kotlin/fr/pokecloud/auth/config/ApplicationConfig.kt
@@ -44,6 +44,8 @@ class ApplicationConfig {
                 .permitAll().requestMatchers(HttpMethod.PUT, "/info").authenticated().requestMatchers("/swagger-ui/**")
                 .permitAll().requestMatchers("/swagger-ui.html").permitAll().requestMatchers("/v3/api-docs*/**")
                 .permitAll()
+                .requestMatchers("/api-docs/").permitAll()
+                .requestMatchers("/api-docs/**").permitAll()
                 .anyRequest().denyAll()
         }.csrf {
             it.disable()
